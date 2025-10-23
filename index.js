8807c9b75c8977bf6079240214facd896821af0b
Rubem <40901612+Rubem1287@users.noreply.github.com>
2025-10-23 18:43:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import routes from "./src/routes.js";
+
+// const express = require("express");
+// const app = express();
+// const port = 3333;
+
+// app.listen(port, (error) => {
+//   if (error) {
+//     console.log("Erro ao conectar.");
+//   }
+//   console.log(`Server online na porta ${port}`);
+// });
